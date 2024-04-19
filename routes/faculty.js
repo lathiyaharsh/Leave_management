77eca155c989d695578033e171ba59f87a70d9ad
lathiyaharsh <patoliya.hl1@gmail.com>
2024-04-19 17:58:54 +0530

    fix logout

--- routes/faculty.js
+++ routes/faculty.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const routes = express.Router();
-const { login, logout } = require("../controller/faculty");
 const verifyToken = require("../middleware/middleware");
 
-
 routes.use(verifyToken(["faculty"]));
-routes.get("/logout", logout);
 
 module.exports = routes;
